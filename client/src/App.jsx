70c702114cfc685b8ee4d324facf772d2395e44e
Jit <jit.oung254343@gmail.com>
2025-01-18 20:14:55 +0700

    finished react part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import DataFetchingComponent from "./renderComponemt";
 
 function App() {
+  /*const [search , setSearch] = useState('') */
+  
 	
 	
 	return (
     <>
-		<div className="flex flex-col items-center font-Prompt">เที่ยวไหนดี</div>
+		<div className="flex flex-col items-center font-Prompt text-5xl mt-10 text-sky-400">เที่ยวไหนดี</div>
 
-    <label htmlFor="searchPlace">ค้นหาที่เที่ยว</label>
-    <input
-    className="border-solid border-2" 
-    type="text" />
 
-    <div className="flex flex-col items-center">
-    <div className="border-solid border-2 w-2/3">
-      <div className="flex flex-row justify-center items-center gap-24">
-        <div className="bg-blue-500">Pic</div>
-        <div className="flex flex-col">
-          <div>title</div>
-          <div>description</div>
-          <div>tag</div>
-          <div className="flex flex-row gap-12">
-            <div>pic1</div>
-            <div>pic2</div>
-            <div>pic3</div>
-          </div>
-        </div>
+    
+    
+     
 
-      </div>
-    </div>
-    </div>
+    
+
+   < DataFetchingComponent/>
 
     </>
 	);
---
+++ client/src/renderComponemt.jsx
@@ -0,0 +1,88 @@
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+
+
+const DataFetchingComponent = () => {
+    const [data, setData] = useState({})
+    const [search , setSearch] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:4001/trips?keywords=')
+            .then(response => response.json())
+            .then(response => setData(response.data));
+    }, []);
+
+
+    return (
+<>
+<div className=" ml-4 flex flex-row gap-4 font-bold" >
+    <label htmlFor="Search">Search Here :</label>
+
+<input
+    className="border-solid border-2" 
+    type="text"
+    placeholder='Search Location'
+    onChange={(e) => setSearch(e.target.value)}
+     />
+</div>
+
+       <div className="flex flex-col items-center gap-5 mt-10 ml-4" >
+       
+        {data.length > 0 ? (
+          data
+          .filter((item) =>{
+            return search.toLowerCase() === ''
+            ? item
+            : item.title.toLowerCase().includes(search)
+          })
+          
+          
+          
+          
+          .map((data, index) => (
+        
+            <div className="w-full">
+            <div className=" ">
+                <div className="flex flex-row justify-start items-center gap-10 w-full">
+                <div className="object-cover w-96" key={index}><img className="rounded-3xl" src={data.photos[0]} alt="" /></div>
+                <div className="flex flex-col justify-between gap-2">
+            <div key={index} className="font-Prompt text-lg font-bold"> {data.title} </div>
+            <div key={index} className="font-Prompt text-sm whitespace-nowrap truncate w-96">{data.description}</div>
+            <div className="font-Prompt text-sky-600 underline text-sm hover:font-bold">อ่านต่อ</div>
+            <div className="flex flex-row gap-2">
+            <div className="font-Prompt text-sm">Tag:</div>
+            <div className="flex flex-row gap-2 font-Prompt text-sm hover:font-bold"> 
+            {data.tags.map((tag) => (
+        <div  key={tag}> {tag} </div>
+      ))}
+                
+
+                 </div>
+                 </div>
+            
+            <div className="flex flex-row gap-4">
+                <div key={index} className="object-contain w-32"><img className="rounded-3xl" src={data.photos[1]} alt="" /></div>
+                <div key={index} className="object-contain w-32"> <img className="rounded-3xl" src={data.photos[2]} alt="" /></div>
+                <div key={index} className="object-contain w-32"><img className="rounded-3xl" src={data.photos[3]} alt="" /></div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+           
+          ))
+        ) : (
+          <p>No blogs found.</p>
+        )}
+        
+        
+
+        
+       </div>
+       </>
+    )
+    
+}
+
+
+export default DataFetchingComponent
